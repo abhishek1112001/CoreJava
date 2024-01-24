57b6c42d86ca2ac900ca09a57272910b2baff968
lenovo <lenovo@lenovo-PC>
2024-01-24 11:20:26 +0530

    commit

---
+++ HomeWork/src/com/edubridge/Homework/HomeworkAssinementTwo.java
@@ -0,0 +1,15 @@
+package com.edubridge.Homework;
+
+import java.util.Scanner;
+
+public class HomeworkAssinementTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		char w =s.next().charAt(0);
+		System.out.printf("Unicode value of '%c' is : %d\n",w,(int)w);
+		s.close();
+	}
+
+}
