12065ac068216012e553b7648ba6f40847b45daf
lenovo <lenovo@lenovo-PC>
2024-03-06 12:22:51 +0530

    update

---
+++ SonarLintDemo/src/SonarLintDemo.java
@@ -0,0 +1,17 @@
+
+
+import java.util.Scanner;
+
+public class SonarLintDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name = "Abhi";
+		
+		System.out.println(name);
+		
+
+	}
+
+}
