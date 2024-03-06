866380ba66242a25387162243c6550022602ea5a
lenovo <lenovo@lenovo-PC>
2024-03-06 21:58:21 +0530

    Update

---
+++ HibernetDemo/src/com/edubridge/main/App.java
@@ -0,0 +1,35 @@
+package com.edubridge.main;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import com.edubridge.entity.Employee;
+
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		
+		Employee e1=new Employee();
+		
+		e1.setEmpName("sam");
+		e1.setAge(21);
+		SessionFactory sessionFactory = null;
+		Session session = null;
+		sessionFactory = HibernateUtil.getSessionFactory();
+		session = sessionFactory.getCurrentSession();
+ //create the session object
+		session.beginTransaction();
+		//create the transaction from the session object
+
+		session.save(e1); 
+		
+		
+		session.getTransaction().commit(); 
+		session.close();
+		
+
+	}
+
+}
