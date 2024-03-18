da894eba7cf66862f9f4b18e3f14784f3936e335
lenovo <lenovo@lenovo-PC>
2024-03-18 12:46:55 +0530

    update

---
+++ SpringWithCollection/src/com/eb/entity/Question.java
@@ -0,0 +1,38 @@
+package com.eb.entity;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class Question {
+
+	private int qId;
+	
+	private String qName;
+	private Map<String,String>answers;
+	public Question(int qId, String qName, Map<String, String> answers) {
+		super();
+		this.qId = qId;
+		this.qName = qName;
+		this.answers = answers;
+	}
+	public Question() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+public void disp() {
+	System.out.println("Question No:"+qId+"\nQuestion:"+qName);
+	
+	System.out.println("*Answers**");
+	
+	Set<Entry<String,String>>set=answers.entrySet();
+	
+	Iterator<Entry<String,String>>itr=set.iterator();
+	while(itr.hasNext()) {
+		Entry<String,String>entry=itr.next();
+		System.out.println(entry.getKey()+" "+entry.getValue());
+	}
+}
+	
+}
